eaf539cf01306231e4d3c8b3a5e78e5de8698403
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-07 21:29:08 -0300

    Fim das aulas sobre inserção e manipulação de vídeos

---
+++ inserindoVideos/script.js
@@ -0,0 +1,34 @@
+let video = document.getElementById("video1");
+
+function retroceder() {
+  video.currentTime -= 10;
+}
+
+function avancar() {
+  video.currentTime += 10;
+}
+
+function diminuir_vel() {
+  video.playbackRate -= 0.1;
+}
+
+function aumentar_vel() {
+  video.playbackRate += 2;
+}
+
+function play() {
+  video.play();
+}
+
+function stop() {
+  video.pause();
+  // video.currentTime = 0;
+}
+
+function esconder() {
+  console.log("oi");
+  let controls = document.querySelector(".controle");
+  if(controls.style.visibility = "hidden"; ){
+    controls.style.visibility = "hidden"; 
+  }
+}
